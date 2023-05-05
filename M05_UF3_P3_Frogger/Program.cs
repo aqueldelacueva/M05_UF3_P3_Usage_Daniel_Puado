bacd1a11b45b20966d982cce9eddfad03ee9535b
aqueldelacueva <deremy797@gmail.com>
2023-05-05 23:32:02 +0200

    comentarios

--- M05_UF3_P3_Frogger/Program.cs
+++ M05_UF3_P3_Frogger/Program.cs
@@ -19,16 +19,16 @@ namespace M05_UF3_P3_Frogger
             // Creamos el mapa del juego
             List<Lane> lanes = new List<Lane>();
             lanes.Add(new Lane(0, false, ConsoleColor.DarkGreen, false, false, 0, Utils.charLogs, new List<ConsoleColor> { ConsoleColor.Green, ConsoleColor.Green }));//Final del mapa
-            lanes.Add(new Lane(1, true, ConsoleColor.Blue, false, true, 0.8f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
-            lanes.Add(new Lane(2, true, ConsoleColor.Blue, false, true, 0.8f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
-            lanes.Add(new Lane(3, true, ConsoleColor.Blue, false, true, 0.8f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
-            lanes.Add(new Lane(4, true, ConsoleColor.Blue, false, true, 0.8f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(1, true, ConsoleColor.Blue, false, true, 0.6f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(2, true, ConsoleColor.Blue, false, true, 0.7f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(3, true, ConsoleColor.Blue, false, true, 0.6f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(4, true, ConsoleColor.Blue, false, true, 0.7f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
             lanes.Add(new Lane(5, true, ConsoleColor.Blue, false, true, 0.8f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
             lanes.Add(new Lane(6, false, ConsoleColor.DarkGreen, false, false, 0, Utils.charLogs, new List<ConsoleColor> { ConsoleColor.Green, ConsoleColor.Green }));//Mitad del mapa
             lanes.Add(new Lane(7, false, ConsoleColor.Black, true, false, 0.1f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
             lanes.Add(new Lane(8, false, ConsoleColor.Black, true, false, 0.1f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
             lanes.Add(new Lane(9, false, ConsoleColor.Black, true, false, 0.1f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
-            lanes.Add(new Lane(10, false, ConsoleColor.Black, true, false, 0.1f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lanes.Add(new Lane(10, false, ConsoleColor.Black, true, false, 0.2f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
             lanes.Add(new Lane(11, false, ConsoleColor.Black, true, false, 0.1f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
             lanes.Add(new Lane(12, false, ConsoleColor.DarkGreen, false, false, 0, Utils.charLogs, new List<ConsoleColor> { ConsoleColor.Green, ConsoleColor.Green }));//Salida
             lanes.Add(new Lane(13, false, ConsoleColor.Black, false, false, 0, Utils.charLogs, new List<ConsoleColor> { ConsoleColor.Green, ConsoleColor.Green }));//Linea para que se vea el fondo negro
@@ -58,7 +58,7 @@ namespace M05_UF3_P3_Frogger
                 // Leemos el input y actualizamos el personaje
                 Vector2Int input = Utils.Input();
                 
-                    gameState = frog.Update(input, lanes);
+                gameState = frog.Update(input, lanes);
                 
                 // Comprobamos si hemos ganado o perdido
                 if (gameState != Utils.GAME_STATE.RUNNING)
